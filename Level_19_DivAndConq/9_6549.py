9fcd2762f6c79cc26d455656032ff2cef0939646
Jung Jongyoon <yoon97610@naver.com>
2021-01-11 00:12:05 +0900

    Update comment of 6549.py

--- Level_19_DivAndConq/9_6549.py
+++ Level_19_DivAndConq/9_6549.py
@@ -17,10 +17,10 @@ class largeRectSolver:
                              which includes start point and 'excludes' end point, [start, end)
 
             returns:
-                returns [A, B] where A = [(h1_a, w1_a), ...],  B = [(h1_b, w1_b), ...]
+                returns [A, D] where A = [(h1_a, w1_a), ...],  D = [(h1_d, w1_d), ...]
                 'A' contains every rectangle informations tuple(height, (largest possible) width)
                     which starts from leftmost location of range of interest
-                Similarly for 'B', rightmost.
+                Similarly for 'D', rightmost.
 
             func divides range of interest as (start, mid) & (mid, end)
             and calls two recursive sub-functions.
@@ -28,27 +28,38 @@ class largeRectSolver:
             Each A, B, C, D can be considered as 'entrance door' of either ranges.
 
             i) Updating self.maxRectArea
-                We suppose rectangles which are located in either [A, B] or [C, D]
-                are already checked if it is self.maxRectArea at base case, or sub-functions.
+                We suppose rectangles which are only located in either [A, B] or [C, D]
+                are already checked if it is largest rectangle during base case or sub-functions.
                 So, We can only consider cases between B and C, where largest rectangle is
                 located on 'mid', divided point. Calculate area, if it is largest, update.
             
             ii) Extend A and D
-                After i), B and C are no longer needed.
+                If rectangle in A can reach (mid, end) range, we must update its information since
+                it can be extended by appending possible rectangle from C ('left' entrance door of (mid, end))
+                Similarly with D and B, too. 
+
+            iii) Return
+                Every information related to B and C are used after i) and ii).
+                So, B and C are no longer needed.
+                We then return [(extended) A, (extended) D]
         """
+
+        # Base case when there is only one item
         if start + 1 == end:
             val = self.arr[start]
             if val > self.maxRectArea:
                 self.maxRectArea = val
             return [[(val, 1)], [(val, 1)]]
 
+        # Split range and call recursive sub-functions
+        # firstRes = [A, B], secondRes = [C, D]
         mid = (end + start) // 2
         firstRes = self.func(start, mid)
         firstLen = mid - start
         secondRes = self.func(mid, end)
         secondLen = end - mid
 
-        # update maxRectArea
+        # i) Updating self.maxRectArea
         for left in firstRes[1]:
             for right in secondRes[0]:
                 height = min(left[0], right[0])
@@ -57,14 +68,15 @@ class largeRectSolver:
                 if val > self.maxRectArea:
                     self.maxRectArea = val
 
+        # ii-1) Extend A where firstRes = [A, B]
         tmp = dict()
         for firstDoor in firstRes[0]:
-            # firstDoor = (7, 2)
+            # for firstDoor = (7, 2) as an example,
             # if we start from leftmost entrance,
             # we can find rectangle height of 7, width of 2
 
-            # for each rectangle, if it can reach firstRes's end,
-            # we can attach it to secondRes.
+            # for each rectangle, if it can reach 'mid',
+            # we can attach it to rectangle from C where secondRes = [C, D]
             if firstDoor[1] == firstLen:
                 for secondDoor in secondRes[0]:
                     height = min(firstDoor[0], secondDoor[0])
@@ -73,7 +85,7 @@ class largeRectSolver:
                         tmp[height] = max(width, tmp[height])
                     except KeyError:
                         tmp[height] = width
-            
+            # Add rectangle that has not been extended to new A list
             height, width = firstDoor[0], firstDoor[1]
             try:
                 tmp[height] = max(width, tmp[height])
@@ -81,6 +93,7 @@ class largeRectSolver:
                 tmp[height] = width
         nextLeftRes = list(zip(tmp.keys(), tmp.values()))
 
+        # ii-2) Extend D where secondRes = [C, D]
         tmp.clear()
         for firstDoor in secondRes[1]:
             if firstDoor[1] == secondLen:
@@ -97,14 +110,13 @@ class largeRectSolver:
                 tmp[height] = max(width, tmp[height])
             except KeyError:
                 tmp[height] = width
-
         nextRightRes = list(zip(tmp.keys(), tmp.values()))
 
+        # iii) Return extended [A, D]
         return [nextLeftRes, nextRightRes]
 
 # N = 10000
 # inputStr = " ".join(list(map(str, list(range(N)))))
-# # # arr = list(map(int, "2 1 4 5 1 3 3".split()))
 # arr = list(map(int, inputStr.split()))
 
 import sys
